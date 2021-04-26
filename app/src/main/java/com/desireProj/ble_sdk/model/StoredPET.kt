ff3e9b434d3e42c3d86d9138b311c145fa7c3f02
ziadTaha <ziadtaha197@gmail.com>
2021-04-26 05:25:05 +0200

    begin model and networks

---
+++ app/src/main/java/com/desireProj/ble_sdk/model/StoredPET.kt
@@ -0,0 +1,16 @@
+package com.desireProj.ble_sdk.model
+
+import com.google.gson.annotations.SerializedName
+import java.time.Duration
+import java.util.*
+
+class StoredPET (
+    @SerializedName("PET")
+    val PETID :String,
+    @SerializedName("distance")
+    val distance:Double,
+    @SerializedName("duration")
+    val duration: Duration,
+    @SerializedName("date")
+    val date : Date
+)
